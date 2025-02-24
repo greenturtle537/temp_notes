96e96b034924d56e5d803885522cc31fd8a6e8e0
Tartarus6 <ivanglott@gmail.com>
2025-02-24 13:48:35 -0500

    added comments to server.js

--- src/server.js
+++ src/server.js
@@ -10,23 +10,22 @@ const NOTES_DIR = 'notes'; // directory where notes db file is stored
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-
 	// Website you wish to allow to connect
 	res.setHeader('Access-Control-Allow-Origin', '*'); //LINE 5
-  
+
 	// Request methods you wish to allow
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  
+
 	// Request headers you wish to allow
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
+
 	// Set to true if you need the website to include cookies in the requests sent
 	// to the API (e.g. in case you use sessions)
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
+
 	// Pass to next layer of middleware
 	next();
-  });
+});
 
 // Initialize SQLite database
 const db = new sqlite3.Database(NOTES_DIR + '/notes.db', (err) => {
